cfc1ce90123c504e2f4063247b71ea051b2c80f0
George Kiboi <gkiboig@gmail.com>
2025-04-30 10:38:42 +0300

    Secure config, add Procfile, ready for Render deployment

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-# Use the affiliate base URL
+import os
 
-TELEGRAM_BOT_TOKEN = "7916450708:AAGxfV58ACrvyuhBaYjvY8iLMGhBRrbxy6g"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TRAVELPAYOUTS_TOKEN = os.getenv("TRAVELPAYOUTS_TOKEN")
+MARKER = os.getenv("MARKER")
 
-
-TRAVELPAYOUTS_TOKEN = "ae0ee3985dc63b8d7c58af22e1cb6e66"
-MARKER = "559862"  # Your Travelpayouts Partner ID
-
-AFFILIATE_BASE_URL = "https://aviasales.tp.st/hgeoGH9E"  # You can use this for link widgets later
+AFFILIATE_BASE_URL = "https://aviasales.tp.st/hgeoGH9E"
